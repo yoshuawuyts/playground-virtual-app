106c968492e7a55dc14377a104fcfadea440226a
Yoshua Wuyts <i@yoshuawuyts.com>
2016-01-21 19:33:03 +1100

    bridge: add

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const history = require('sheet-router/history')
+const bridge = require('sheet-router/bridge')
 const sheetRouter = require('sheet-router')
 const createApp = require('virtual-app')
 const vdom = require('virtual-dom')
@@ -7,34 +8,30 @@ const xtend = require('xtend')
 
 const hx = hyperx(vdom.h)
 
-// routing
-const router = sheetRouter('/404', function (r) {
-  return [
-    r('/', function (params, h, state) {
-      console.log('/')
-      return template(h, state)
-    }),
-    r('/double', function (params, h, state) {
-      console.log('/double')
-      console.log(state)
-      return template(h, state)
-    }),
-    r('/404', function (params, h, state) {
-      console.log('/404')
-      return template(h, state)
-    })
-  ]
-})
-history(router)
-
 // initialize and attach
 const app = createApp(document.body, vdom)
-const initialState = { count: 0, location: document.location.href }
+const initialState = { count: 0, mod: 1, location: document.location.href }
 const render = app.start(modifyState, initialState)
-render((state) => router(state.location, app.h, state))
+const router = createRouter()
+history((href) => app.store({ type: 'location', location: href }))
+bridge(render, (state) => router(state.location, app.h))
+
+// routing
+function createRouter () {
+  return sheetRouter('/404', function (r, t) {
+    return [
+      t('/', template(singleHead)),
+      t('/double', template(multiHead)),
+      t('/404', template(errorHead))
+    ]
+  })
+}
 
 // manage state changes
 function modifyState (action, state) {
+  if (action.type === 'location') {
+    return xtend(state, { location: action.location })
+  }
   if (action.type === 'increment') {
     return xtend(state, { count: state.count + 1 })
   }
@@ -43,14 +40,38 @@ function modifyState (action, state) {
   }
 }
 
-function template (h, state) {
-  return hx`
-    <main>
-      <a href="/">single</a>
-      <a href="/double?foo=bar">double</a>
-      <div>${state.count}</div>
-      <button onclick=${app.send({ type: 'decrement' })}>-1</button>
-      <button onclick=${app.send({ type: 'increment' })}>+1</button>
-    </main>
-  `
+// render views
+function template (head) {
+  return function template (params, h, state) {
+    return hx`
+      <main>
+        <a href="/">single</a>
+        <a href="/double?foo=bar">double</a>
+        ${head(params, h, state)}
+        <p>modifier is ${state.mod}</p>
+        <div>count: ${state.count}</div>
+        <button onclick=${app.send({ type: 'decrement' })}>
+          -${state.mod}
+        </button>
+        <button onclick=${app.send({ type: 'increment' })}>
+          +${state.mod}
+        </button>
+      </main>
+    `
+  }
+}
+
+// head component
+function singleHead (params, h, state) {
+  return hx`<h1>super single</h1>`
+}
+
+// another head component
+function multiHead (params, h, state) {
+  return hx`<h1>double up!</h1>`
+}
+
+// signal an error
+function errorHead (params, h, state) {
+  return hx`<h1>OH NO!</h1>`
 }
